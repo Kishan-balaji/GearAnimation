bc2a70565edec5745af847db837798eb4775a0c4
Kishan Balaji <99073598+Kishan-balaji@users.noreply.github.com>
2024-10-15 16:51:13 +0530

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media.Animation;
+using System.Windows.Forms; // For accessing Screen class
+
+namespace GearAnimation
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
